743c15c6e0fa4365365fe4e9484e13dea2cacb8b
Rdhn12 <111635737+Rdhn12@users.noreply.github.com>
2024-10-21 06:02:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -19,16 +19,16 @@ fi
 printf "\033c"
 printf "${GREEN}╭────────────────────────────────────────────────────────────────────────────────╮${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                             Pterodactyl VPS EGG                                │${NC}\n"
+printf "${GREEN}│                              Ekuzikaa VPS EGG                                │${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                           ${RED}© 2021 - 2024 ${PURPLE}ysdragon${GREEN}                               │${NC}\n"
+printf "${GREEN}│                        ${RED}© 2021 - 2024 ${PURPLE}ysdragon${GREEN}                               │${NC}\n"
 printf "${GREEN}│                                                                                │${NC}\n"
 printf "${GREEN}╰────────────────────────────────────────────────────────────────────────────────╯${NC}\n"
 printf "                                                                                               \n"
-printf "root@MyVPS:${DIR}#                                                                             \n"
+printf "root@Ekuzikaa:${DIR}#                                                                             \n"
 
 run_cmd() {
-    read -p "root@MyVPS:$DIR# " CMD
+    read -p "root@Ekuzikaa:$DIR# " CMD
     eval "$CMD"
     
     # Update DIR after executing command
@@ -37,22 +37,8 @@ run_cmd() {
         DIR="~${PWD#/root}"
     fi
     
-    printf "root@MyVPS:$DIR# \n"
+    printf "root@Ekuzikaa:$DIR# \n"
     run_user_cmd
-}
-
-run_user_cmd() {
-    read -p "user@MyVPS:$DIR# " CMD2
-    eval "$CMD2"
-    
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@MyVPS:$DIR# \n"
-    run_cmd
 }
 
 run_cmd
